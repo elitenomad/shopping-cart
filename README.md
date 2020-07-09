c9d71cd50c34ccacfec8bbff3400c4a438a4c0a6
pranava s balugari <stalin.pranava@gmail.com>
2020-07-09 10:39:09 +1000

    Update Readme , update the text across the files

--- README.md
+++ README.md
@@ -20,7 +20,34 @@ bin/console
 
 ## Usage
 
-TODO: Write usage instructions here
+```
+# Set up Discounts
+discount_free = Shopping::Cart::FreeTypeDiscount.new(label: 'Free VGA', type: 'freeProduct', targetSku: 'vga')
+discount_qtyDiscount = Shopping::Cart::QtyTypeDiscount.new(label: '3 for 2 discount', type: 'qtyDiscount', discountQty: 3, discountQtyVolume: 1)
+discount_priceDiscount = Shopping::Cart::PriceTypeDiscount.new(label: 'Bulk Discount', type: 'priceDiscount', discountQty: 3, discountPrice: 499.99)
+
+# Set up items or Listings - Attach a discount to each item
+item_1 = Shopping::Cart::Item.new(sku: 'ipd', name: 'Super iPad', price: 549.99)
+item_1.addTodiscount(discount_priceDiscount)
+item_2 = Shopping::Cart::Item.new(sku: 'mbp', name: 'Macbook pro', price: 1399.99)
+item_2.addTodiscount(discount_free)
+item_3 = Shopping::Cart::Item.new(sku: 'atv', name: 'Apple TV', price: 109.50)
+item_3.addTodiscount(discount_qtyDiscount)
+item_4 = Shopping::Cart::Item.new(sku: 'vga', name: 'VGA Adapter', price: 30.00)
+
+# Setup inventory
+inventory = []
+inventory << item_1
+inventory << item_2
+inventory << item_3
+inventory << item_4
+
+# Initialize the Checkout class with inventory
+s = Shopping::Cart::Checkout.new(inventory)
+s.scan("ipd")
+s.total
+
+```
 
 ## Spec
 
@@ -31,3 +58,14 @@ rspec spec/
 ```
 
 ## Design
+
+- Took ```bundle gem``` help to create a folder structure. It would be a great idea to pluck this function to a gem.
+- Reason for having separate class for discounts is that, if we provide an admin interface to sales manager (for that model class - given there is a backend implementation), he/she can play around with the values. 
+- 
+- 
+
+
+**Assumptions**
+- Items are unlimited in stock. 
+- Item can have one Discount at any given point in time.
+- 
--- spec/shopping/cart/checkout_spec.rb
+++ spec/shopping/cart/checkout_spec.rb
@@ -1,11 +1,11 @@
 RSpec.describe Shopping::Cart::Checkout do
-  let(:special_free) { Shopping::Cart::FreeTypeDiscount.new(label: 'Free VGA', type: 'freeProduct', targetSku: 'vga') }
-  let(:special_qtyDiscount) { Shopping::Cart::QtyTypeDiscount.new(label: '3 for 2 Special', type: 'qtyDiscount', discountQty: 3, discountQtyVolume: 1) }
-  let(:special_priceDiscount) { Shopping::Cart::PriceTypeDiscount.new(label: 'Bulk Discount', type: 'priceDiscount', discountQty: 3, discountPrice: 499.99) }
+  let(:discount_free) { Shopping::Cart::FreeTypeDiscount.new(label: 'Free VGA', type: 'freeProduct', targetSku: 'vga') }
+  let(:discount_qtyDiscount) { Shopping::Cart::QtyTypeDiscount.new(label: '3 for 2 discount', type: 'qtyDiscount', discountQty: 3, discountQtyVolume: 1) }
+  let(:discount_priceDiscount) { Shopping::Cart::PriceTypeDiscount.new(label: 'Bulk Discount', type: 'priceDiscount', discountQty: 3, discountPrice: 499.99) }
 
-  let(:item_1) { Shopping::Cart::Item.new(sku: 'ipd', name: 'Super iPad', price: 549.99).tap{|t| t.addToDiscount(special_priceDiscount) }}
-  let(:item_2) { Shopping::Cart::Item.new(sku: 'mbp', name: 'Macbook pro', price: 1399.99).tap{|t| t.addToDiscount(special_free) } }
-  let(:item_3) { Shopping::Cart::Item.new(sku: 'atv', name: 'Apple TV', price: 109.50).tap{|t| t.addToDiscount(special_qtyDiscount) } }
+  let(:item_1) { Shopping::Cart::Item.new(sku: 'ipd', name: 'Super iPad', price: 549.99).tap{|t| t.addToDiscount(discount_priceDiscount) }}
+  let(:item_2) { Shopping::Cart::Item.new(sku: 'mbp', name: 'Macbook pro', price: 1399.99).tap{|t| t.addToDiscount(discount_free) } }
+  let(:item_3) { Shopping::Cart::Item.new(sku: 'atv', name: 'Apple TV', price: 109.50).tap{|t| t.addToDiscount(discount_qtyDiscount) } }
   let(:item_4) { Shopping::Cart::Item.new(sku: 'vga', name: 'VGA Adapter', price: 30.00) }
 
   let(:inventory) { [item_1, item_2, item_3, item_4] }
